c6581f84aa68eeac2cb3c712bc493fab980ba47e
DKabucho <danielkabucho2004@gmail.com>
2023-10-04 12:34:59 +0300

    Header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
-    <header>
-      <h1>Adoption Center</h1>
-    </header>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+        <li>
+          <Link to="/register">Register</Link>
+        </li>
+        {/* Add more navigation links as needed */}
+      </ul>
+    </nav>
   );
 }
 
